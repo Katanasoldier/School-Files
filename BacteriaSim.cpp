67f676e25da0258adfdc84372b7c45221a675952
Katanasoldier <akor3618@gmail.com>
2025-05-09 14:19:10 +0200

    Add files via upload

---
+++ BacteriaSim.cpp
@@ -0,0 +1,24 @@
+﻿// 09.05.2025
+
+// EN: Program that simulates the multiplication of bacteria over time.
+// PL: Program symulujacy rozwoj bakterii przez czas.
+
+
+#include <iostream>
+
+using namespace std;
+
+int Population = 1;
+int Day = 1;
+
+int main()
+{
+
+    while(Population<10000000){
+        Population *= 2;
+        Day++;
+    }
+
+    cout << "On day " << Day << " there were " << Population << " bacteria.";
+
+}
